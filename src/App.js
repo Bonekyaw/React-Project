a8cf3161decdd5041af5c877fc1fbbb06a2cc6bb
Phone Nyo <phonenai2014@gmail.com>
2019-06-04 16:01:40 +0630

    Class extends Component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import React,{useState} from 'react';
-import Hello from './Hello';
-
-function App() {
-
-	const [isRed, setRed] = useState(false);
-	const [count, setCount] = useState(0);
-
-	const [users,setUsers] = useState([
-			{name:"Bone Kyaw", message:"Hey I am fine"},
-			{name:"Moe Moe", message:"Now I am cooking"},
-			{name:"Sithu ", message:"Hey I am learning"}
-		]);
-
-	const increment = () => {
-			setCount(count + 1);
-			setRed(!isRed);
-		};
-
-  return (
-  	<div >
-	    <h1 className={isRed ? 'red' : ''}> This is App Component {count}</h1>
-	    <button onClick={increment}> Increment </button>
-	    <div className="appExtra">
-		    {users.map( user => (
-		    	<Hello name={user.name} message={user.message} />
-		    ))}
-	    </div>
-
-  	</div>
-  );
+import React, {Component} from 'react';
+ 
+class App extends Component  {
+	render() {
+		return (
+			<div> 
+				<h1>Hello React</h1>
+			</div>
+			)
+	}
 }
-
 export default App;
--- src/Hello.js
+++ src/Hello.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import './App.css';
+// import React from 'react';
+// import './App.css';
 
-function Hello(props) {
+// function Hello(props) {
 
-	return (
-		<div className="hello"> 
-			<h1> {props.name} </h1>
-			<p> {props.message} </p>
-		</div>
-	);
-}
-export default Hello;
+// 	return (
+// 		<div className="hello"> 
+// 			<h1> {props.name} </h1>
+// 			<p> {props.message} </p>
+// 		</div>
+// 	);
+// }
+// export default Hello;
+
+
+// /*
+// App JS 
+
+// import React,{useState} from 'react';
+// import Hello from './Hello';
+
+// function App() {
+
+// 	const [isRed, setRed] = useState(false);
+// 	const [count, setCount] = useState(0);
+
+// 	const [users,setUsers] = useState([
+// 			{name:"Bone Kyaw", message:"Hey I am fine"},
+// 			{name:"Moe Moe", message:"Now I am cooking"},
+// 			{name:"Sithu ", message:"Hey I am learning"}
+// 		]);
+
+// 	const increment = () => {
+// 			setCount(count + 1);
+// 			setRed(!isRed);
+// 		};
+
+//   return (
+//   	<div >
+// 	    <h1 className={isRed ? 'red' : ''}> This is App Component {count}</h1>
+// 	    <button onClick={increment}> Increment </button>
+// 	    <div className="appExtra">
+// 		    {users.map( user => (
+// 		    	<Hello name={user.name} message={user.message} />
+// 		    ))}
+// 	    </div>
+
+//   	</div>
+//   );
+// }
+
+// export default App;
+
+//  */

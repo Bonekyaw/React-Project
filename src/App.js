6fa1e0a77d7ede44870c782b068d7e4ad1578589
Phone Nyo <phonenai2014@gmail.com>
2019-06-04 16:27:49 +0630

    this.state constructor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
  
 class App extends Component  {
+
+	constructor(){
+		super();
+		this.state = {
+			name: 'Bone Kyaw',
+			age : 23,
+		}
+	}
 	render() {
 		return (
 			<div> 
 				<h1>Hello React</h1>
+				<p>Name : {this.state.name}</p>
+				<p>Age: {this.state.age}</p>
 			</div>
 			)
 	}

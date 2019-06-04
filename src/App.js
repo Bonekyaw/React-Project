e44872179513e0138631eb08b5c3f3490191cf42
Phone Nyo <phonenai2014@gmail.com>
2019-06-04 11:53:30 +0630

    State Change

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import Hello from './Hello';
 
 function App() {
 
+	const [isRed, setRed] = useState(false);
+	const [count, setCount] = useState(0);
+
 	const [users,setUsers] = useState([
 			{name:"Bone Kyaw", message:"Hey I am fine"},
 			{name:"Moe Moe", message:"Now I am cooking"},
 			{name:"Sithu ", message:"Hey I am learning"}
 		]);
 
-
+	const increment = () => {
+			setCount(count + 1);
+			setRed(!isRed);
+		};
 
   return (
   	<div >
-	    <h1> This is App Component </h1>
+	    <h1 className={isRed ? 'red' : ''}> This is App Component {count}</h1>
+	    <button onClick={increment}> Increment </button>
 	    <div className="appExtra">
 		    {users.map( user => (
 		    	<Hello name={user.name} message={user.message} />
